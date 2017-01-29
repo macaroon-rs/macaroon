937a548bf9b624d20a418356050d388d17a35052
Jack Lund <jackl@geekheads.net>
2017-01-28 21:46:22 -0600

    Switch to rustc-serialize for base64

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,6 @@
-extern crate base64;
+extern crate rustc_serialize as serialize;
 extern crate sodiumoxide;
 
 pub mod error;
 pub mod macaroon;
 pub mod serialization;
-
-// #[cfg(test)]
-// mod tests {
-//     #[test]
-//     fn test() {
-//         unimplemented!();
-//     }
-// }
